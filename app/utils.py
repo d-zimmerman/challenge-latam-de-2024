e4a7e5f974bc788b21a230c1f61d83330062bc4b
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-07 18:37:57 -0300

    Add memory profile wrapper in utils module
    This wrapper is needed to send memory profile to the project's logs

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,7 @@ import io
 import pstats
 
 from logger import Logger
+from memory_profiler import profile
 
 
 def profile_function(func):
@@ -28,6 +29,29 @@ def profile_function(func):
 
         # Enviar los resultados del buffer al logger
         profiler_logger.info("cProfile results:\n%s", text_buffer.getvalue())
+
+        return result
+
+    return wrapper
+
+
+def memory_profile_logging_wrapper(func):
+    """Run Memory Profile over the function."""
+    profiler_logger = Logger.get_app_logger("MemoryProfile")
+
+    def wrapper(*args, **kwargs):
+        """Wrap the function to run Memory Profile over it."""
+        # Capturing output using StringIO
+        stream = io.StringIO()
+
+        # Overriding the stream in memory_profiler
+        profiled_func = profile(stream=stream)(func)
+
+        # Execute the function with memory profiling
+        result = profiled_func(*args, **kwargs)
+
+        # Log the memory profiler results
+        profiler_logger.info("Memory profiler results:\n%s", stream.getvalue())
 
         return result
 
