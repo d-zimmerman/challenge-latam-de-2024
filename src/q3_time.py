3a8b7bbc8e2db5730a8acceb9b083094f0631ab3
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 20:01:50 -0300

    Update q3 time module
    Add docstrings and common imports

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,29 @@
+"""This module contains the q3_time function."""
+
 from typing import List, Tuple
 
+from app.logger import Logger
+from app.utils import memory_profile_logging_wrapper, profile_function
+
+module_logger = Logger.get_app_logger("Q3-TIME")
+
+
+@profile_function
+@memory_profile_logging_wrapper
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    """Answer question 3 efficiently in time.
+
+    Parameters
+    ----------
+    file_path : str
+        Path of the json file to be loaded
+
+    Returns
+    -------
+    List[Tuple[datetime.date, str]]
+        List of the 10 most mentioned users and
+        the number of times they had been mentioned.
+    """
+    module_logger.info("Starting Q3-TIME...")
+
+    module_logger.info("Ending Q3-TIME...")
