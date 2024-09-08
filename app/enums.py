e61183d05988245c02ca8b372af9435aac1835dd
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 20:38:47 -0300

    Update enums module
    Add LoggerModuleEnum

--- app/enums.py
+++ app/enums.py
@@ -1,1 +1,17 @@
 """This module contains enums for the app."""
+
+from strenum import StrEnum
+
+
+class LoggerModuleEnum(StrEnum):
+    """Enum class for the name of the logger's modules."""
+
+    MAIN_APP = 'MAIN'
+    TIME_PROFILER = 'C_PROFILER'
+    MEMORY_PROFILE = 'MEMORY_PROFILE'
+    Q1_TIME = "Q1-TIME"
+    Q2_TIME = "Q2-TIME"
+    Q3_TIME = "Q3-TIME"
+    Q1_MEMORY = "Q1-MEMORY"
+    Q2_MEMORY = "Q2-MEMORY"
+    Q3_MEMORY = "Q3-MEMORY"
