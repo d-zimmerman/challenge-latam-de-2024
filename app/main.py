e878bf8a6529cbfd84d782e51a188d79e0a72b5f
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 20:02:35 -0300

    Update main module
    Add q3 time function to main function

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from src.q1_time import q1_time
 from src.q2_memory import q2_memory
 from src.q2_time import q2_time
 from src.q3_memory import q3_memory
+from src.q3_time import q3_time
 
 module_logger = Logger.get_app_logger("MAIN")
 
@@ -32,6 +33,9 @@ def main() -> None:
 
     result = q3_memory(file_path)
     module_logger.info(f"Q3-MEMORY result:\n{result}")
+
+    result = q3_time(file_path)
+    module_logger.info(f"Q3-TIME result:\n{result}")
 
     module_logger.info("Ending main.")
 
