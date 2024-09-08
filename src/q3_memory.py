b6497d13a55eecd407d899db512a0c99f6dc6173
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 19:57:33 -0300

    Update q3 memory function
    Update q3 function code

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,5 +1,8 @@
 """This module contains the q3_memory function."""
 
+import heapq
+import json
+from collections import defaultdict
 from typing import List, Tuple
 
 from app.logger import Logger
@@ -13,6 +16,18 @@ module_logger = Logger.get_app_logger("Q2-MEMORY")
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
     """Answer question 3 efficiently in memory.
 
+        In this function we had two possible paths:
+            1. Use a RegEx to find the mentions (@) in the context/text
+              of the tweet.
+            2. Use the `mentionedUsers` key in the JSON and leaving to
+              the API of Tweeter the identification of those mentions
+
+        I decided to go with the second approach.
+
+        This approach also include reading the JSON file line by line
+        and counting how many times the same user is mentioned.
+        At the end the TOP 10 users is found.
+
     Parameters
     ----------
     file_path : str
@@ -25,5 +40,22 @@ def q3_memory(file_path: str) -> List[Tuple[str, int]]:
         the number of times they had been mentioned.
     """
     module_logger.info("Starting Q3-MEMORY...")
+    users_counter = defaultdict(int)
+    module_logger.info("Counting mentioned users")
+    with open(file_path, 'r', encoding="utf-8") as file:
+        for line in file:
+            mentioned_users = json.loads(line).get("mentionedUsers")
+
+            if mentioned_users is None:
+                continue
+
+            for user in mentioned_users:
+                if user.get("username") is not None:
+                    users_counter[user.get("username")] += 1
+
+    module_logger.info("Finding TOP 10 mentioned users")
+    top_10_users = heapq.nlargest(10, users_counter.items(), key=lambda x: x[1])
 
     module_logger.info("Ending Q3-MEMORY...")
+
+    return top_10_users
