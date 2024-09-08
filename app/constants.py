fead1be241d7f903d82f4518607920b7e861cb8f
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 17:58:06 -0300

    Update constants module
    Add ALL_EMOJIS constant to be used in q2 time function

--- app/constants.py
+++ app/constants.py
@@ -2,8 +2,13 @@
 
 from pathlib import Path
 
+import emoji
+
 PROJECT_ROOT = Path(__file__).parent.parent
 
 DATA_DIR = PROJECT_ROOT / "data"
 
 FILENAME = "farmers-protest-tweets-2021-2-4.json"
+
+# Get all emojis in emoji library
+ALL_EMOJIS = list(emoji.EMOJI_DATA.keys())
