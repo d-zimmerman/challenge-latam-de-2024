449d710a5f657ca481b223735ec9d080036e60cd
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 11:19:09 -0300

    Update q1_memory function
    Remove unnecessary intermediate variables and add some  statements.
    
    Update dict to defaultdict for a shorter code after seeing it doesn't affect the memory efficiency of the function
    
    Remove the usage of the Garbage Collector. It was not doing nothing impressive for the memory usage and it was adding delay to the code. Python handles garbage collection in a great way by itself

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,8 +1,8 @@
 """This module contains the q1_memory function."""
 
-import gc
 import heapq
 import json
+from collections import defaultdict
 from datetime import datetime
 from typing import List, Tuple
 
@@ -45,80 +45,77 @@ def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
         (the most tweeted date will appear first).
     """
     module_logger.info("Starting Q1-MEMORY...")
-    # Initialize dict that will store the number of tweets per day.
+
     module_logger.info("Finding TOP 10 dates")
-    tweet_count_by_date = {}
+    # Initialize dict that will store the number of tweets per day.
+    tweet_count_by_date = defaultdict(int)
     # Load json line by line so memory usage is optimized
-    # By this we load in memory only the line that will be processed.
-    # Previous line is discarded after processing. Next line waits in the buffer.
     with open(file_path, "r", encoding="utf-8") as file:
         for line in file:
             tweet = json.loads(line)
-
-            # Extract date from JSON
-            date = tweet["date"].split("T")[0]
-
-            # `defaultdict` could be used to avoid this check
-            # but we prioritize memory as much as possible.
-            if date not in tweet_count_by_date:
-                tweet_count_by_date[date] = 0
-            tweet_count_by_date[date] += 1
+            # Extract date and parse it
+            # Increment tweet counter by date
+            tweet_count_by_date[
+                datetime.strptime(tweet['date'].split('T')[0], r'%Y-%m-%d').date()
+            ] += 1
 
     # Find TOP 10 dates
-    # We use heapq instead of sorted. In this way we iterate over
-    # the dates and only keep the top 10. Sorted will try to sort the list
-    # loading it completely in memory.
+    # We use heapq instead of sorted:
+    #   The heap method only stores the top n elements at any time,
+    #   whereas the sorting method requires storage of all N elements.
+    #   This means that for large datasets, the heap method's
+    #   memory usage remains constant
     top_10_dates = heapq.nlargest(
         10,
         tweet_count_by_date.keys(),
         key=lambda date: tweet_count_by_date[date],  # noqa: F821
     )
     del tweet_count_by_date
-    gc.collect()  # Force garbage collection
     module_logger.info("TOP 10 dates found.")
 
-    # Initialize dict that will store the most active user.
-    user_tweet_count_by_date = {}
-
     module_logger.info("Finding most active user for each date")
+    # Initialize dict that will store the most active user.
+    result = []
+    user_tweet_count_by_date = defaultdict(lambda: defaultdict(int))
     # Load json line by line so memory usage is optimized
-    # By this we load in memory only the line that will be processed.
-    # Previous line is discarded after processing. Next line waits in the buffer.
     with open(file_path, "r", encoding="utf-8") as file:
         for line in file:
             tweet = json.loads(line)
 
-            # Extract date from JSON
-            date = tweet['date'].split('T')[0]
-
-            # If date is not included in the TOP 10, skip loop
-            if date not in top_10_dates:
+            # If date is not in `top_10_dates` skip iteration
+            # If the top_10_dates was larger (more than 10 dates)
+            # we should think of another data type to store these dates.
+            # to make this `not in` clause more memory efficient.
+            # Given that we only have 10 dates, we are going to consider
+            # this improvement as unnecessary for now.
+            if (
+                datetime.strptime(tweet['date'].split('T')[0], r'%Y-%m-%d').date()
+                not in top_10_dates
+            ):
                 continue
 
-            username = tweet["user"]['username']
+            user_tweet_count_by_date[
+                datetime.strptime(tweet['date'].split('T')[0], r'%Y-%m-%d').date()
+            ][tweet["user"]['username']] += 1
 
-            # `defaultdict` could be used to avoid this check
-            # but we prioritize memory as much as possible.
-            if date not in user_tweet_count_by_date:
-                user_tweet_count_by_date[date] = {}
-            if username not in user_tweet_count_by_date[date]:
-                user_tweet_count_by_date[date][username] = 0
-            # Increment counter
-            user_tweet_count_by_date[date][username] += 1
-
-    result = []
-    for date in top_10_dates:
-        # pylint: disable=W0640
-        # heapq is triggering pylint without reason
-        top_user = heapq.nlargest(
-            1,
-            user_tweet_count_by_date[date].keys(),
-            key=lambda username: user_tweet_count_by_date[date][username],
-        )
-        # pylint: enable=W0640
-        del user_tweet_count_by_date[date]
-        gc.collect()  # Force garbage collection
-        result.append((date, top_user))
+        for top_date in top_10_dates:
+            # Find TOP 10 dates
+            # We use heapq instead of sorted:
+            #   The heap method only stores the top n elements at any time,
+            #   whereas the sorting method requires storage of all N elements.
+            #   This means that for large datasets, the heap method's
+            #   memory usage remains constant
+            # pylint: disable=W0640
+            top_user = heapq.nlargest(
+                1,
+                user_tweet_count_by_date[top_date].keys(),
+                key=lambda username: user_tweet_count_by_date[top_date][  # noqa: F821
+                    username
+                ],
+            )[0]
+            # pylint: enable=W0640
+            result.append((top_date, top_user))
+        del user_tweet_count_by_date
     module_logger.info("Most active user for each date found")
     module_logger.info("Finishing Q1-MEMORY...")
     return result
