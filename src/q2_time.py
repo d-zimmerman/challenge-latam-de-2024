4d78de1e24009bb158eda08d006c17c9e66c5082
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 16:16:32 -0300

    Update Q2 time module
    Add docstrings and common imports

--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,28 @@
+"""This module contains the q2_time function."""
+
 from typing import List, Tuple
 
+from app.logger import Logger
+from app.utils import memory_profile_logging_wrapper, profile_function
+
+module_logger = Logger.get_app_logger("Q2-TIME")
+
+
+@profile_function
+@memory_profile_logging_wrapper
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    """Answer question 2 efficiently in time.
+
+    Parameters
+    ----------
+    file_path : str
+        Path of the json file to be loaded
+
+    Returns
+    -------
+    List[Tuple[datetime.date, str]]
+        List of the 10 most used emojis and
+        the number of times they had been used.
+    """
+    module_logger.info("Starting Q2-TIME...")
+    module_logger.info("Finishing Q2-TIME...")
