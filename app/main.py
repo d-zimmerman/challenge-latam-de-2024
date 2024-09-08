5bba3f7e3dd0b1b1d9b1efd68826f60dcac1417c
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 14:03:15 -0300

    Update main module
    Include q1_time function

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from logger import Logger
 
 from app.constants import DATA_DIR, FILENAME
 from src.q1_memory import q1_memory
+from src.q1_time import q1_time
 
 module_logger = Logger.get_app_logger("MAIN")
 
@@ -12,8 +13,13 @@ def main() -> None:
     """Run main script of the app."""
     module_logger.info("Starting main.")
 
-    result = q1_memory(DATA_DIR / FILENAME)
+    file_path = DATA_DIR / FILENAME
+
+    result = q1_memory(file_path)
     module_logger.info(f"Q1-MEMORY result:\n{result}")
+
+    result = q1_time(file_path)
+    module_logger.info(f"Q1-TIME result:\n{result}")
 
     module_logger.info("Ending main.")
 
