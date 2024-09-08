75b54fa879f6fe8f0a7a2821aebd23a98d4f1539
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 15:03:07 -0300

    Update Q2 memory module and function

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,28 @@
+"""This module contains the q2_memory function."""
+
 from typing import List, Tuple
 
+from app.logger import Logger
+from app.utils import memory_profile_logging_wrapper, profile_function
+
+module_logger = Logger.get_app_logger("Q2-MEMORY")
+
+
+@profile_function
+@memory_profile_logging_wrapper
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    """Answer question 2 efficiently in memory.
+
+    Parameters
+    ----------
+    file_path : str
+        Path of the json file to be loaded
+
+    Returns
+    -------
+    List[Tuple[datetime.date, str]]
+        List of the 10 most used emojis and
+        the number of times they had been used.
+    """
+    module_logger.info("Starting Q2-MEMORY...")
+    module_logger.info("Finishing Q2-MEMORY...")
