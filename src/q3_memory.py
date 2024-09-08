dc12674e1070dfffcaa63b088c7c810d8e0e561e
d-zimmerman <dzimmerman2611@gmail.com>
2024-09-08 19:40:25 -0300

    Update q3_function module
    Add docstrings and common imports

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,29 @@
+"""This module contains the q3_memory function."""
+
 from typing import List, Tuple
 
+from app.logger import Logger
+from app.utils import memory_profile_logging_wrapper, profile_function
+
+module_logger = Logger.get_app_logger("Q2-MEMORY")
+
+
+@profile_function
+@memory_profile_logging_wrapper
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    """Answer question 3 efficiently in memory.
+
+    Parameters
+    ----------
+    file_path : str
+        Path of the json file to be loaded
+
+    Returns
+    -------
+    List[Tuple[datetime.date, str]]
+        List of the 10 most mentioned users and
+        the number of times they had been mentioned.
+    """
+    module_logger.info("Starting Q3-MEMORY...")
+
+    module_logger.info("Ending Q3-MEMORY...")
